d4ed42cd681717882990349b2e35886a0e2a45c6
Daniel <davilareko@gmail.com>
2025-09-29 17:29:49 +0200

    Fix read and write

--- api/v1/index.ts
+++ api/v1/index.ts
@@ -36,12 +36,53 @@ class MountPointManager {
 
 export { MountPointManager };
 
-const fs_read = promisify(fs_regular.read);
-const fs_write = promisify(fs_regular.write);
-
 const S_IFMT = 0o170000;  // bitmask for the file type
 const S_IFDIR = 0o040000; // directory
 const S_IFREG = 0o100000; // regular file
+
+function serializeError(err: { [key: string]: any }) {
+    if (!(err instanceof Error)) {
+        return err;
+    }
+    const serialized = {
+        // @ts-ignore
+        name: err.name,
+        // @ts-ignore
+        message: err.message,
+        stack: err.stack,
+        ...err,
+    } as { [key: string]: any };
+    for (const key of Object.getOwnPropertyNames(err)) {
+        if (!(key in serialized)) {
+            serialized[key] = (err as any)[key];
+        }
+    }
+    return serialized;
+}
+
+function sanitizeFsError(err: any, removePrefix: string) {
+    if (!(err instanceof Error)) return err;
+
+    const clone = { ...serializeError(err) };
+
+    if (clone.path && clone.path.startsWith(removePrefix)) {
+        clone.path = clone.path.replace(removePrefix, '');
+    }
+
+    if (clone.dest && clone.dest.startsWith(removePrefix)) {
+        clone.dest = clone.dest.replace(removePrefix, '');
+    }
+
+    if (clone.message) {
+        clone.message = clone.message.replace(removePrefix, '');
+    }
+    if (clone.stack) {
+        clone.stack = clone.stack.replace(new RegExp(removePrefix, 'g'), '');
+    }
+
+    return clone;
+}
+
 
 function handleWs(req: Request): Response {
     const { socket, response } = Deno.upgradeWebSocket(req);
@@ -56,109 +97,119 @@ function handleWs(req: Request): Response {
         const responseMessageBuf = new SpecBuffer();
         control_message.write(responseMessageBuf, opCode, head.id);
         // console.log(responseMessageBuf.getOffset());
-        switch (opCode) {
-            case "ls": {
-                const dirPath = spec[opCode].read(specBuf).path;
-                const securePath = MountPointManager.resolveSecurePath(currentMountPoint, dirPath);
-                response_spec[opCode].write(responseMessageBuf, await fs.readdir(securePath));
-                break;
-            }
-            case "stat": {
-                const filePath = spec[opCode].read(specBuf).path;
-                const securePath = MountPointManager.resolveSecurePath(currentMountPoint, filePath);
-                const rawStat = await fs.stat(securePath);
-                const stat: StatOutput = {
-                    size: rawStat.size,
-                    mode: rawStat.mode,
-                    mtime: BigInt(rawStat.mtime.getTime()),
-                    ctime: BigInt(rawStat.ctime.getTime()),
-                    atime: BigInt(rawStat.atime.getTime())
+        try {
+            switch (opCode) {
+                case "ls": {
+                    const dirPath = spec[opCode].read(specBuf).path;
+                    const securePath = MountPointManager.resolveSecurePath(currentMountPoint, dirPath);
+                    response_spec[opCode].write(responseMessageBuf, await fs.readdir(securePath));
+                    break;
                 }
-                response_spec[opCode].write(responseMessageBuf, stat);
-                break;
-            }
-            case "read": {
-                const options = spec[opCode].read(specBuf);
-                const filePath = options.path;
-                const securePath = MountPointManager.resolveSecurePath(currentMountPoint, filePath);
-                const length = options.end - options.start;
-                const fileData = new Uint8Array(length);
-                // const fd = await fs_regular.promises.open(`${currentMountpoint}/${filePath}`, "r");
-                const fd = await fs.open(securePath, "r");
-                // fs_regular.readSync(fd, fileData, 0, length, options.start);
-                // fs_regular.closeSync(fd);
-                await fs_read(fd.fd, fileData, 0, length, options.start);
-                await fd.close();
-                response_spec[opCode].write(responseMessageBuf, fileData);
-                break;
-            }
-            case "touch": {
-                const options = spec[opCode].read(specBuf);
-                const filePath = options.path;
-                const securePath = MountPointManager.resolveSecurePath(currentMountPoint, filePath);
-                const stat = options.stat;
-                // console.log(stat);
-                await fs.utimes(securePath, Number(stat.atime) / 1000, Number(stat.mtime) / 1000);
-                response_spec[opCode].write(responseMessageBuf, true);
-                break;
-            }
-            case "write": {
-                const options = spec[opCode].read(specBuf);
-                const filePath = options.path;
-                const securePath = MountPointManager.resolveSecurePath(currentMountPoint, filePath);
-                const fd = await fs.open(securePath, "w+");
-                await fs_write(fd.fd, options.data, 0, options.data.byteLength, options.offset);
-                await fd.close();
-                // const fd = fs_regular.openSync(`${currentMountpoint}/${filePath}`, "w+");
-                // fs_regular.writeSync(fd, Buffer.from(options.data.buffer), 0, options.data.byteLength, options.offset);
-                // fs_regular.closeSync(fd);
-                response_spec[opCode].write(responseMessageBuf, true);
-                break;
-            }
-            case "unlink": {
-                const filePath = spec[opCode].read(specBuf).path;
-                const securePath = MountPointManager.resolveSecurePath(currentMountPoint, filePath);
-                await fs.unlink(securePath);
-                response_spec[opCode].write(responseMessageBuf, true);
-                break;
-            }
-            case "new": {
-                const options = spec[opCode].read(specBuf);
-                const securePath = MountPointManager.resolveSecurePath(currentMountPoint, options.path);
-                let madeDir = false;
-                switch (options.opt.mode & S_IFMT) {
-                    case S_IFDIR:
-                        await fs.mkdir(securePath);
-                        madeDir = true;
-                        break;
-                    case S_IFREG:
-                        await fs.writeFile(securePath, "");
-                        break;
+                case "stat": {
+                    const filePath = spec[opCode].read(specBuf).path;
+                    const securePath = MountPointManager.resolveSecurePath(currentMountPoint, filePath);
+                    const rawStat = await fs.stat(securePath);
+                    const stat: StatOutput = {
+                        size: rawStat.size,
+                        mode: rawStat.mode,
+                        mtime: BigInt(rawStat.mtime.getTime()),
+                        ctime: BigInt(rawStat.ctime.getTime()),
+                        atime: BigInt(rawStat.atime.getTime())
+                    }
+                    response_spec[opCode].write(responseMessageBuf, stat);
+                    break;
                 }
-                response_spec[opCode].write(responseMessageBuf, {
-                    size: madeDir ? 4096 : 0,
-                    mode: options.opt.mode,
-                    mtime: BigInt(Date.now()),
-                    ctime: BigInt(Date.now()),
-                    atime: BigInt(Date.now()),
-                });
-                break;
+                case "read": {
+                    const options = spec[opCode].read(specBuf);
+                    const filePath = options.path;
+                    const securePath = MountPointManager.resolveSecurePath(currentMountPoint, filePath);
+                    const length = options.end - options.start;
+                    console.log(options, length);
+                    const fileData = new Uint8Array(length);
+                    // const fd = await fs_regular.promises.open(`${currentMountpoint}/${filePath}`, "r");
+                    const fd = await fs.open(securePath, "r");
+                    // fs_regular.readSync(fd, fileData, 0, length, options.start);
+                    // fs_regular.closeSync(fd);
+                    // await fs_read(fd.fd, fileData, 0, length, options.start);
+                    await fd.read(fileData, 0, length, options.start);
+                    await fd.close();
+                    response_spec[opCode].write(responseMessageBuf, fileData);
+                    break;
+                }
+                case "touch": {
+                    const options = spec[opCode].read(specBuf);
+                    const filePath = options.path;
+                    const securePath = MountPointManager.resolveSecurePath(currentMountPoint, filePath);
+                    const stat = options.stat;
+                    // console.log(stat);
+                    await fs.utimes(securePath, Number(stat.atime) / 1000, Number(stat.mtime) / 1000);
+                    response_spec[opCode].write(responseMessageBuf, true);
+                    break;
+                }
+                case "write": {
+                    const options = spec[opCode].read(specBuf);
+                    const filePath = options.path;
+                    const securePath = MountPointManager.resolveSecurePath(currentMountPoint, filePath);
+                    const fd = await fs.open(securePath, "r+");
+                    console.log(options);
+                    // await fs_write(fd.fd, options.data, 0, options.data.byteLength, options.offset);
+                    await fd.write(options.data, 0, options.data.byteLength, options.offset);
+                    await fd.close();
+                    // const fd = fs_regular.openSync(`${currentMountpoint}/${filePath}`, "w+");
+                    // fs_regular.writeSync(fd, Buffer.from(options.data.buffer), 0, options.data.byteLength, options.offset);
+                    // fs_regular.closeSync(fd);
+                    response_spec[opCode].write(responseMessageBuf, true);
+                    break;
+                }
+                case "unlink": {
+                    const filePath = spec[opCode].read(specBuf).path;
+                    const securePath = MountPointManager.resolveSecurePath(currentMountPoint, filePath);
+                    await fs.unlink(securePath);
+                    response_spec[opCode].write(responseMessageBuf, true);
+                    break;
+                }
+                case "new": {
+                    const options = spec[opCode].read(specBuf);
+                    const securePath = MountPointManager.resolveSecurePath(currentMountPoint, options.path);
+                    let madeDir = false;
+                    switch (options.opt.mode & S_IFMT) {
+                        case S_IFDIR:
+                            await fs.mkdir(securePath);
+                            madeDir = true;
+                            break;
+                        case S_IFREG:
+                            await fs.writeFile(securePath, "");
+                            break;
+                    }
+                    response_spec[opCode].write(responseMessageBuf, {
+                        size: madeDir ? 4096 : 0,
+                        mode: options.opt.mode,
+                        mtime: BigInt(Date.now()),
+                        ctime: BigInt(Date.now()),
+                        atime: BigInt(Date.now()),
+                    });
+                    break;
+                }
+                case "rmdir": {
+                    const filePath = spec[opCode].read(specBuf).path;
+                    const securePath = MountPointManager.resolveSecurePath(currentMountPoint, filePath);
+                    await fs.rmdir(securePath);
+                    response_spec[opCode].write(responseMessageBuf, true);
+                    break;
+                }
+                case "move": {
+                    const options = spec[opCode].read(specBuf);
+                    const secureSrcPath = MountPointManager.resolveSecurePath(currentMountPoint, options.src_path);
+                    const secureDstPath = MountPointManager.resolveSecurePath(currentMountPoint, options.dst_path);
+                    await fs.rename(secureSrcPath, secureDstPath);
+                    response_spec[opCode].write(responseMessageBuf, true);
+                    break;
+                }
             }
-            case "rmdir": {
-                const filePath = spec[opCode].read(specBuf).path;
-                const securePath = MountPointManager.resolveSecurePath(currentMountPoint, filePath);
-                await fs.rmdir(securePath);
-                response_spec[opCode].write(responseMessageBuf, true);
-                break;
-            }
-            case "move": {
-                const options = spec[opCode].read(specBuf);
-                const secureSrcPath = MountPointManager.resolveSecurePath(currentMountPoint, options.src_path);
-                const secureDstPath = MountPointManager.resolveSecurePath(currentMountPoint, options.dst_path);
-                await fs.rename(secureSrcPath, secureDstPath);
-                response_spec[opCode].write(responseMessageBuf, true);
-                break;
-            }
+        }
+        catch (e: any) {
+            console.log(e);
+            responseMessageBuf.emplaceIntoBuffer((new TextEncoder().encode(`ERR:${JSON.stringify(sanitizeFsError(e, currentMountPoint))}`)));
         }
         socket.send(responseMessageBuf.getBuffer());
     });
